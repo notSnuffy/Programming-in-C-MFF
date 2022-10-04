5efb351b04c7c76daa6992c61f86b2fce8570b99
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-10-05 01:52:54 +0200

    Add lab1/hello.cpp and lab1/input.cpp

--- labs/lab1/hello.cpp
+++ labs/lab1/hello.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-
+    for (int i = 0; i < argc; ++i){
+        std::cout << "Hello " << argv[i] << "\n";
+    }
 }
--- labs/lab1/input.cpp
+++ labs/lab1/input.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
+int EuclidianAlgorithm(int a, int b){
+    while (b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
 
+int main() {
+    int a;
+    int b;
+    std::cin >> a;
+    std::cin >> b;
+    int result = EuclidianAlgorithm(a, b);
+    std::cout << result;
 }
