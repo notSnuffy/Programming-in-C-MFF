6c69f5a15e78feb2242945ba9af979d52e66ced6
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-10-31 06:28:31 +0100

    Add lab3: linked_list.cpp

--- labs/lab3/linked_list.cpp
+++ labs/lab3/linked_list.cpp
@@ -7,22 +7,41 @@ List new_list() {
 }
 
 Node * at(List list, int index) {
+        
+    Node* current = list.head;
+ 
+    int count = 0;
+    while (current != nullptr) {
+        if (count == index)
+            return (current);
+        count++;
+        current = current->next;
+    }
     return nullptr;
 }
 
 int size(List list) {
-    return 0;
+    int count = 0;
+    Node * current = list.head;
+    while(current != nullptr){
+        ++count;
+        current = current->next;
+    }
+    return count;
 }
 
-void push_front(List list, int value) {
-
+void push_front(List & list, int value) {
+    Node * node = new Node();
+    node->value = value;
+    node->next = list.head;
+    list.head = node;
 }
 
-Node * insert(List list, int index, int value) {
+Node * insert(List & list, int index, int value) {
     return nullptr;
 }
 
-Node * erase(List list, int value) {
+Node * erase(List & list, int value) {
     return nullptr;
 }
 
