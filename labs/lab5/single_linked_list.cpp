42bb62d76d733cc16dfb9d6aa81c442946053d47
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-11-07 12:21:24 +0100

    Lab5: add lab5 assignments

--- labs/lab5/single_linked_list.cpp
+++ labs/lab5/single_linked_list.cpp
@@ -1,5 +1,104 @@
-# include "list.h"
+#include "list.h"
 
-namespace lab5 {
+namespace lab5
+{
+
+    List::Node *List::at(int index) const
+    {
+        Node *current = this->head;
+
+        int count = 0;
+        while (current != nullptr)
+        {
+            if (count == index)
+                return (current);
+            count++;
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    int List::size() const
+    {
+        int count = 0;
+        Node *current = this->head;
+        while (current != nullptr)
+        {
+            ++count;
+            current = current->next;
+        }
+        return count;
+    }
+
+    void List::push_front(int value)
+    {
+        Node *node = new Node();
+        node->value = value;
+        node->next = this->head;
+        this->head = node;
+    }
+
+    void List::insert(int index, int value)
+    {
+        Node *node = new Node();
+        node->value = value;
+
+        if (index == 0)
+        {
+            push_front(value);
+            return;
+        }
+
+        if (head == nullptr)
+            return;
+
+        if (index < size())
+        {
+            Node *temp = head;
+            for (int i = 1; i < index; i++)
+            {
+                temp = temp->next;
+            }
+            node->next = temp->next;
+            temp->next = node;
+            return;
+        }
+
+        if (index == size())
+        {
+            Node *temp = head;
+
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = node;
+        }
+    }
+
+    List::Node *List::erase(int value)
+    {
+        if (head == nullptr)
+            return nullptr;
+        if (head->value == value)
+        {
+            Node *retNode = head;
+            head = head->next;
+            return retNode;
+        }
+        Node *temp = head;
+        while (temp->next != nullptr && value != temp->next->value)
+        {
+            temp = temp->next;
+        }
+
+        if (temp->next != nullptr)
+        {
+            Node *deleted = temp->next;
+            temp->next = temp->next->next;
+            return deleted;
+        }
+        return nullptr;
+    }
 
 }
--- labs/lab5/test_list.cpp
+++ labs/lab5/test_list.cpp
@@ -1,6 +1,151 @@
 #include <gtest/gtest.h>
 
-#include "single_linked_list.h"
+#include "list.h"
 
 using namespace lab5;
 
+TEST(TestClassNewList, Empty)
+{
+    List list{};
+    ASSERT_EQ(list.at(0), nullptr);
+    ASSERT_EQ(list.size(), 0);
+}
+
+TEST(TestClassPushFront, AddOne)
+{
+    List list{};
+    list.push_front(42);
+    ASSERT_EQ(list.size(), 1);
+
+    List::Node *node = list.at(0);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(node->value, 42);
+}
+
+TEST(TestClassPushFront, AddMultiple)
+{
+    List list{};
+
+    for (int i = 9; i >= 0; --i)
+    {
+        list.push_front(i);
+    }
+    EXPECT_EQ(list.size(), 10);
+
+    for (int i = 0; i <= 9; ++i)
+    {
+        List::Node *node = list.at(i);
+        ASSERT_NE(node, nullptr);
+        ASSERT_EQ(node->value, i);
+    }
+}
+
+TEST(TestClassInsert, InsertToEmpty)
+{
+    List list{};
+    list.insert(0, 42);
+    EXPECT_EQ(list.size(), 1);
+
+    List::Node *node = list.at(0);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(node->value, 42);
+}
+
+TEST(TestClassInsert, InsertToFront)
+{
+    List list{};
+
+    list.push_front(1);
+    list.push_front(2);
+    ASSERT_EQ(list.size(), 2);
+
+    list.insert(0, 42);
+    ASSERT_EQ(list.size(), 3);
+
+    List::Node *node = list.at(0);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(node->value, 42);
+}
+
+TEST(TestClassInsert, InsertToBack)
+{
+    List list{};
+
+    list.push_front(1);
+    list.push_front(2);
+    EXPECT_EQ(list.size(), 2);
+
+    int back_index = list.size();
+    list.insert(back_index, 42);
+    EXPECT_EQ(list.size(), 3);
+
+    List::Node *node = list.at(back_index);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(node->value, 42);
+}
+
+TEST(TestClassInsert, InsertToMiddle)
+{
+    List list{};
+
+    list.push_front(1);
+    list.push_front(2);
+    EXPECT_EQ(list.size(), 2);
+
+    list.insert(1, 42);
+    EXPECT_EQ(list.size(), 3);
+
+    List::Node *node = list.at(1);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(node->value, 42);
+}
+
+TEST(TestClassErase, EraseEmpty)
+{
+    List list{};
+
+    ASSERT_EQ(list.erase(10), nullptr);
+}
+
+TEST(TestClassHeadErase, EraseHead)
+{
+    List list{};
+
+    list.push_front(10);
+    list.push_front(11);
+
+    List::Node *deleted = list.erase(11);
+
+    EXPECT_EQ(list.size(), 1);
+    ASSERT_EQ(deleted->value, 11);
+    ASSERT_EQ(list.at(0)->value, 10);
+}
+
+TEST(TestClassEraseMiddle, EraseMiddle)
+{
+    List list{};
+
+    list.push_front(10);
+    list.push_front(11);
+    list.push_front(12);
+    list.push_front(13);
+
+    List::Node * deleted = list.erase(12);
+    EXPECT_EQ(list.size(), 3);
+    ASSERT_EQ(deleted->value, 12);
+    ASSERT_EQ(list.at(0)->next->value, 11);
+}
+
+TEST(TestClassEraseEnd, EraseEnd) {
+    List list{};
+
+    list.push_front(10);
+    list.push_front(11);
+    list.push_front(12);
+    list.push_front(13);
+
+    List::Node * deleted = list.erase(10);
+    EXPECT_EQ(list.size(), 3);
+    ASSERT_EQ(deleted->value, 10);
+    ASSERT_EQ(list.at(2)->next, nullptr);
+}
