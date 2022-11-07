2c45a732a4cd4ed513545f1d904e9794098ecb64
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-11-07 12:59:27 +0100

    Lab3: update linked_list.cpp

--- labs/lab3/linked_list.cpp
+++ labs/lab3/linked_list.cpp
@@ -1,29 +1,91 @@
 #include "linked_list.h"
 
-namespace lab3 {
+namespace lab3
+{
 
-List new_list() {
-    return List{};
-}
+    List new_list()
+    {
+        return List{};
+    }
 
-Node * at(List list, int index) {
-    return nullptr;
-}
+    Node *at(List list, int index)
+    {
 
-int size(List list) {
-    return 0;
-}
+        Node *current = list.head;
 
-void push_front(List list, int value) {
+        int count = 0;
+        while (current != nullptr)
+        {
+            if (count == index)
+                return (current);
+            count++;
+            current = current->next;
+        }
+        return nullptr;
+    }
 
-}
+    int size(List list)
+    {
+        int count = 0;
+        Node *current = list.head;
+        while (current != nullptr)
+        {
+            ++count;
+            current = current->next;
+        }
+        return count;
+    }
 
-Node * insert(List list, int index, int value) {
-    return nullptr;
-}
+    void push_front(List &list, int value)
+    {
+        Node *node = new Node();
+        node->value = value;
+        node->next = list.head;
+        list.head = node;
+    }
 
-Node * erase(List list, int value) {
-    return nullptr;
-}
+    void insert_to_list(List &list, int index, int value)
+    {
+        Node *node = new Node();
+        node->value = value;
+
+        if (index == 0)
+        {
+            push_front(list, value);
+            return;
+        }
+
+        if (list.head == nullptr)
+            return;
+
+        if (index < size(list))
+        {
+            Node *temp = list.head;
+            for (int i = 1; i < index; i++)
+            {
+                temp = temp->next;
+            }
+            node->next = temp->next;
+            temp->next = node;
+            return;
+        }
+
+        if (index == size(list))
+        {
+            Node *temp = list.head;
+
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = node;
+            return;
+        }
+    }
+
+    Node *erase(List &list, int value)
+    {
+        return nullptr;
+    }
 
 }
