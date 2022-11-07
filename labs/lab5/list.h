589fce7de8d675979fef000596e979a6895bb7ee
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-11-07 13:11:04 +0100

    Lab5: add list.h

--- labs/lab5/list.h
+++ labs/lab5/list.h
@@ -1,5 +1,21 @@
 #pragma once
 
 namespace lab5 {
+    
+    class List {
+        public:
+            struct Node{
+                Node * next;
+                int value;
+            };
 
+            Node * at(int index) const;
+            int size() const;
+            void push_front(int value);
+            void insert(int index, int value);
+            Node * erase(int value);
+
+        private:
+            Node * head;     
+    };
 }
