fa1843be568ccfc81333ffce7a3d117415e429f6
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-12-12 12:24:21 +0100

    Lab8: add histogram

--- labs/lab8/histogram.cpp
+++ labs/lab8/histogram.cpp
@@ -4,5 +4,5 @@
 
 int main() {
     Histogram h{std::cin};
-    std::cout << h.char_count();
+    std::cout << h.char_count('c');
 }
--- labs/lab8/histogram.h
+++ labs/lab8/histogram.h
@@ -4,7 +4,12 @@ struct Histogram {
     Histogram(std::istream & input) : input(input) {}
 
     std::size_t char_count(char c) const {
-        return 0;
+        char x;
+        std::size_t count;
+        while(input >> x) {
+            if (x == c) ++count;
+        }
+        return count;
     }
 
     std::size_t word_length(std::size_t length) const {
