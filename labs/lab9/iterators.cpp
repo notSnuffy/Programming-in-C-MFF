a9dbc9de5c60f907f8f6c852295ae99ad18f04d4
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2022-12-12 12:25:25 +0100

    Lab9: add iterators.cpp

--- labs/lab9/iterators.cpp
+++ labs/lab9/iterators.cpp
@@ -6,37 +6,65 @@
 
 using namespace std;
 
-void print(vector<int>::const_iterator begin, vector<int>::const_iterator end) {
+void print(vector<int>::const_iterator begin, vector<int>::const_iterator end)
+{
     cerr << "call: void print(vector<int>::const_iterator begin, vector<int>::const_iterator end)" << endl;
+    for (auto it = begin; it != end; ++it)
+    {
+        std::cout << *it << ' ';
+    }
 }
 
-template<typename Iterator>
-void print(Iterator begin, Iterator end) {
+template <typename Iterator>
+void print(Iterator begin, Iterator end)
+{
     cerr << "call: void print(Iterator begin, Iterator end)" << endl;
+    for (auto it = begin; it != end; ++it)
+    {
+        std::cout << *it << ' ';
+    }
 }
 
-void bubblesort(vector<int>::iterator begin, vector<int>::iterator end) {
+void bubblesort(vector<int>::iterator begin, vector<int>::iterator end)
+{
     cerr << "call: void bubblesort(vector<int>::iterator begin, vector<int>::iterator end)" << endl;
 }
 
-template<typename Iterator>
-void bubblesort(Iterator begin, Iterator end) {
+template <typename Iterator>
+void bubblesort(Iterator begin, Iterator end)
+{
     cerr << "call: void void bubblesort(Iterator begin, Iterator end)" << endl;
+    for (auto it = begin; it != end - 1; ++it)
+    {
+        for (auto it2 = begin; it2 != end - it - 1; ++it2)
+        {
+            if (*it > *it2)
+            {
+                swap(it, it2);
+            }
+        }
+    }
 }
 
-struct S {
+struct S
+{
     int i;
 };
 
-int main(void) {
+int main(void)
+{
 #if 1
     {
         cout << "Task 1: print vector of ints" << endl;
+        vector<int> v = {1, 2};
+        print(v.cbegin(), v.cend());
     }
 #endif
 #if 0
     {
         cout << "Task 2: bubblesort vector of ints" << endl;
+        vector<float> v = {1, 2};
+        print(v.cbegin(), v.cend());
     }
 #endif
 #if 0
