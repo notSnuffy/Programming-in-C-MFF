852a840223aee2641e8e2430820573d98a1c9403
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-03-29 13:28:43 +0200

    Add BST insert and has_value tests

---
+++ Final/tests/BST_test.cpp
@@ -0,0 +1,102 @@
+#include "BST.h"
+#include <gtest/gtest.h>
+
+TEST(TestInsertValid, RootNode)
+{
+
+    BinarySearchTree<int> tree;
+
+    ASSERT_TRUE(tree.insert(10));
+}
+
+TEST(TestInsertValid, LeftLeafNode) {
+    BinarySearchTree<int> tree;
+    tree.insert(10);
+    ASSERT_TRUE(tree.insert(8));
+}
+
+TEST(TestInsertValid, RightLeafNode)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(10);
+    ASSERT_TRUE(tree.insert(8));
+}
+
+TEST(TestInsertValid, DeeplyNestedTree) {
+    BinarySearchTree<int> tree;
+    ASSERT_TRUE(tree.insert(3));
+    ASSERT_TRUE(tree.insert(8));
+    ASSERT_TRUE(tree.insert(10));
+    ASSERT_TRUE(tree.insert(14));
+    ASSERT_TRUE(tree.insert(13));
+    ASSERT_TRUE(tree.insert(1));
+    ASSERT_TRUE(tree.insert(6));
+    ASSERT_TRUE(tree.insert(4));
+    ASSERT_TRUE(tree.insert(7));
+}
+
+TEST(TestInsertInvalid, DeeplyNestedTree)
+{
+    BinarySearchTree<int> tree;
+    ASSERT_TRUE(tree.insert(3));
+    ASSERT_FALSE(tree.insert(3));
+    ASSERT_TRUE(tree.insert(8));
+    ASSERT_TRUE(tree.insert(10));
+    ASSERT_TRUE(tree.insert(14));
+    ASSERT_TRUE(tree.insert(13));
+    ASSERT_FALSE(tree.insert(13));
+    ASSERT_TRUE(tree.insert(1));
+    ASSERT_TRUE(tree.insert(6));
+    ASSERT_TRUE(tree.insert(4));
+    ASSERT_TRUE(tree.insert(7));
+    ASSERT_FALSE(tree.insert(7));
+}
+
+TEST(TestHasValueValid, DeeplyNestedTree) {
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    ASSERT_TRUE(tree.has_value(8));
+    tree.insert(3);
+    ASSERT_TRUE(tree.has_value(3));
+    tree.insert(10);
+    ASSERT_TRUE(tree.has_value(10));
+    tree.insert(14);
+    ASSERT_TRUE(tree.has_value(14));
+    tree.insert(13);
+    ASSERT_TRUE(tree.has_value(13));
+    tree.insert(1);
+    ASSERT_TRUE(tree.has_value(1));
+    tree.insert(6);
+    ASSERT_TRUE(tree.has_value(6));
+    tree.insert(4);
+    ASSERT_TRUE(tree.has_value(4));
+    tree.insert(7);
+    ASSERT_TRUE(tree.has_value(7));
+}
+
+TEST(TestHasValueInvalid, EmptyTree) {
+    BinarySearchTree<int> tree;
+    ASSERT_FALSE(tree.has_value(10));
+}
+
+TEST(TestHasValueInvalid, RootOnly)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(99);
+    ASSERT_FALSE(tree.has_value(10));
+}
+
+TEST(TestHasValueInvalid, DeeplyNestedTree)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    ASSERT_FALSE(tree.has_value(100));
+}
