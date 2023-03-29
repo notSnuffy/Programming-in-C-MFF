dccadce4e7b22747cce42f8a774769ed117fbca9
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-03-29 15:23:44 +0200

    Add new tests for testing BST maximum

--- Final/tests/BST_test.cpp
+++ Final/tests/BST_test.cpp
@@ -101,7 +101,7 @@ TEST(TestHasValueInvalid, DeeplyNestedTree)
     ASSERT_FALSE(tree.has_value(100));
 }
 
-TEST(TestMaximum, Valid) {
+TEST(TestMaximum, NoParamValid) {
     BinarySearchTree<int> tree;
     tree.insert(8);
     tree.insert(3);
@@ -116,8 +116,101 @@ TEST(TestMaximum, Valid) {
     ASSERT_EQ(maximum, 14);
 }
 
-TEST(TestMaximum, Invalid) {
+TEST(TestMaximum, NoParamInvalid) {
     BinarySearchTree<int> tree;
     int maximum = tree.maximum().value_or(-12312312);
     ASSERT_EQ(maximum, -12312312);
+}
+
+TEST(TestMaximum, WithParamInvalidEmptyTree)
+{
+    BinarySearchTree<int> tree;
+    int maximum = tree.maximum(10).value_or(-12312312);
+    ASSERT_EQ(maximum, -12312312);
+}
+
+TEST(TestMaximum, WithParamInvalidNoNode)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    int maximum = tree.maximum(20).value_or(-12312312);
+    ASSERT_EQ(maximum, -12312312);
+}
+
+TEST(TestMaximum, WithParamValidRoot) {
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    int maximum = tree.maximum(8).value();
+    ASSERT_EQ(maximum, 14);
+}
+
+TEST(TestMaximum, WithParamValidMiddleNode)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    int maximum = tree.maximum(3).value();
+    ASSERT_EQ(maximum, 7);
+}
+
+TEST(TestMaximum, WithParamValidLeafNode)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    int maximum = tree.maximum(4).value();
+    ASSERT_EQ(maximum, 4);
+}
+
+TEST(TestMinimum, Valid)
+{
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(13);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(7);
+    int minimum = tree.minimum().value();
+    ASSERT_EQ(minimum, 1);
+}
+
+TEST(TestMinimum, Invalid)
+{
+    BinarySearchTree<int> tree;
+    int minimum = tree.minimum().value_or(-12312312);
+    ASSERT_EQ(minimum, -12312312);
 }
