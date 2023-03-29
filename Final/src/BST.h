4f8a6ba6993488a0443889ec5c14801eaaec695a
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-03-29 12:18:57 +0200

    Implement insert operation for nested nodes

--- Final/src/BST.h
+++ Final/src/BST.h
@@ -1,18 +1,60 @@
 #include "Node.h"
 #include <memory>
+#include <functional>
 
-template<typename T>
-class BinarySearchTree {
-    private:
+template <typename T, typename Comparator = std::less<T>>
+class BinarySearchTree
+{
+private:
     std::unique_ptr<Node<T>> root;
 
+    bool is_less_than(const T &a, const T &b, Comparator compare = Comparator{})
+    {
+        return compare(a, b);
+    }
 
-    public:
-    bool insert(T value) {
-        if(root == nullptr) {
+public:
+    bool insert(const T &value)
+    {
+        // Insert to root
+        if (root == nullptr)
+        {
             root = std::make_unique<Node<T>>(value);
             return true;
         }
-        return false;
-    } 
+
+        Node<T> *current = root.get();
+        Node<T> *parent = nullptr;
+
+        while (current != nullptr)
+        {
+            parent = current;
+            if (is_less_than(value, current->value))
+            {
+                current = current->left.get();
+            }
+            else if (is_less_than(current->value, value))
+            {
+                current = current->right.get();
+            }
+            // Do not insert -> duplicate value
+            else
+            {
+                return false;
+            }
+        }
+
+        // We have current as leaf node -> we insert somewhere to parent node
+        // Here we figure out where
+        if (is_less_than(value, parent->value))
+        {
+            parent->left = std::make_unique<Node<T>>(value);
+            parent->left->parent = parent;
+        } else {
+            parent->right = std::make_unique<Node<T>>(value);
+            parent->right->parent = parent;
+        }
+
+        return true;
+    }
 };
